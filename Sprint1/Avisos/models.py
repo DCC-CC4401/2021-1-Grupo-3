f6fab7d489914df198babb63f4664212a08abc1f
ricardo-valdivia <ricardo.valdivia22@gmail.com>
2021-04-27 17:12:15 -0400

    Se añaden primeros modelos de django

--- Sprint1/Avisos/models.py
+++ Sprint1/Avisos/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    Nombre = models.CharField(max_length=30)
+    Nombre_De_Usuario = models.CharField(max_length=30)
+    Fecha_De_Nacimiento = models.DateField()
+    Email = models.EmailField()
+    Contraseña = models.CharField(max_length=15)
+
+class Aviso(models.Model):
+    Id = models.IntegerField()
+    Motivo = models.CharField(max_length=15)
+    Titulo = models.CharField(max_length=30)
+    Comuna = models.CharField(max_length=30)
+    Fecha = models.DateTimeField()
+    Tipo_Animal = models.CharField(max_length=30)
+    Sexo = models.CharField(max_length=15)
+    Nombre_De_Usuario = models.CharField(max_length=30)
+    Descripcion = models.CharField(max_length=250)
+    Foto = models.ImageField()
+    Estado_del_Aviso = models.BooleanField()
